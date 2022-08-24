5563bc457d37fdf4435e21c839580fde00e48fa3
ANGELOMOTTAQUISPE <47892949+ANGELOMOTTAQUISPE@users.noreply.github.com>
2022-08-24 16:45:33 -0500

    v2.4 24-08

--- src/main/java/businessbanking/repo/IAccountRepo.java
+++ src/main/java/businessbanking/repo/IAccountRepo.java
@@ -1,9 +1,14 @@
 package businessbanking.repo;
 
 import businessbanking.model.Account;
+import businessbanking.model.Credit;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface IAccountRepo extends ReactiveMongoRepository<Account, String> {
+    @Query(value = "{'client.documentNumber' : ?0, accountType: ?1 }")
+    Flux<Credit> findByAccountClient(String documentNumber, String accountType);
 }
--- src/main/java/businessbanking/service/impl/AccountServiceImpl.java
+++ src/main/java/businessbanking/service/impl/AccountServiceImpl.java
@@ -1,9 +1,15 @@
 package businessbanking.service.impl;
 
+import businessbanking.controller.CreditController;
+import businessbanking.exception.ModelNotFoundException;
 import businessbanking.model.Account;
+import businessbanking.model.Credit;
 import businessbanking.model.Fee;
 import businessbanking.repo.IAccountRepo;
+import businessbanking.repo.IClientRepo;
 import businessbanking.service.IAccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -14,24 +20,60 @@ import java.time.LocalDateTime;
 
 @Service
 public class AccountServiceImpl implements IAccountService {
+    private static final Logger logger = LoggerFactory.getLogger(CreditController.class);
     @Autowired
     private IAccountRepo repo;
+    @Autowired
+    private IClientRepo repoClient;
 
     public Mono<Account> register(Account obj) {
-        Fee fee = new Fee();
-        String AccountType=obj.getAccountType().toString();
-        if (AccountType.equals("a")){
-            fee.setMonthlyMovement(5);
-            obj.setFee(fee);
-        } else if (AccountType.equals("cc")) {
-            fee.setMaintenanceCommission(200.0);
-            obj.setFee(fee);
-        } else if (AccountType.equals("pf")) {
-            fee.setDate(LocalDateTime.now());
-            fee.setMonthlyMovement(1);
-            obj.setFee(fee);
-        }
-        return repo.save(obj);
+
+
+        String documentNumber =obj.getClient().getDocumentNumber();
+        return repoClient.findByDocumentNumber(documentNumber)
+                .flatMap( cl -> {
+                    Fee fee = new Fee();
+                    String AccountType=obj.getAccountType().toString();
+                    if (AccountType.equals("a")){
+                        fee.setMonthlyMovement(5);
+                        obj.setFee(fee);
+                    } else if (AccountType.equals("cc")) {
+                        fee.setMaintenanceCommission(200.0);
+                        obj.setFee(fee);
+                    } else if (AccountType.equals("pf")) {
+                        fee.setDate(LocalDateTime.now());
+                        fee.setMonthlyMovement(1);
+                        obj.setFee(fee);
+                    }
+
+                    if(cl.getClientType().equals("personal")){
+                        logger.info("personal");
+                        Flux<Credit> lista = repo.findByAccountClient(documentNumber, AccountType);
+                        Mono<Long> count = lista.count();
+                        return count
+                                .flatMap( c->{
+                                    logger.info("-- : "+c);
+                                    if(c > 0){
+                                        logger.info("1: "+c);
+                                        throw new ModelNotFoundException(" El cliente personal ya tiene una cuenta: "+c);
+                                        //return Mono.just("El cliente ya tiene un credito");
+                                    }else{
+                                        logger.info("2: "+c);
+                                        return repo.save(obj);
+                                    }
+                                });
+                    }else if(cl.getClientType().equals("empresarial")){
+                        if( AccountType.equals("a") || AccountType.equals("pf") ){
+                            throw new ModelNotFoundException(" El cliente empresarial ya tiene una cuenta: ");
+                        }else{
+                            //logger.info("empresarial: " + obj.getIdCredit() + " - " +  obj.getCreditCardNumber());
+                            return repo.save(obj);
+                        }
+                    }else{
+                        return Mono.just(obj);
+                    }
+                })
+                .then(  Mono.just(obj) );
     }
 
     public Mono<Account> modify(Account obj) {
